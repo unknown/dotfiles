f3019a57ba16dfed6397907c63344cd222af6e86
David Mo <44306450+unknown@users.noreply.github.com>
2024-08-13 01:20:48 -0400

    Add treesitter to neovim

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "cpp",
+        "elixir",
+        "markdown",
+        "javascript",
+        "typescript",
+        "json",
+        "html",
+        "css",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
