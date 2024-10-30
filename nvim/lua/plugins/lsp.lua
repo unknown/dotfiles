ef971dd605f23941f9e99b5455e2856fb3f3785b
David Mo <44306450+unknown@users.noreply.github.com>
2024-10-30 13:04:48 -0400

    Update blink.cmp keymap

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -25,9 +25,7 @@ return {
     dependencies = "rafamadriz/friendly-snippets",
     version = "v0.*",
     opts = {
-      keymap = {
-        accept = "<CR>",
-      },
+      keymap = "super-tab",
       highlight = {
         use_nvim_cmp_as_default = true,
       },
