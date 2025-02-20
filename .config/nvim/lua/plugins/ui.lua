9d8ff1ec0f5579e1094a84873dffea9baabc9d14
David Mo <david@dmo.ooo>
2025-02-19 22:15:44 -0500

    add bufferline buffer keybinds

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -48,6 +48,16 @@ return {
       { "]b",         "<cmd>BufferLineCycleNext<cr>" },
       { "[B",         "<cmd>BufferLineMovePrev<cr>" },
       { "]B",         "<cmd>BufferLineMoveNext<cr>" },
+      { "<leader>1",  "<cmd>BufferLineGoToBuffer 1<cr>" },
+      { "<leader>2",  "<cmd>BufferLineGoToBuffer 2<cr>" },
+      { "<leader>3",  "<cmd>BufferLineGoToBuffer 3<cr>" },
+      { "<leader>4",  "<cmd>BufferLineGoToBuffer 4<cr>" },
+      { "<leader>5",  "<cmd>BufferLineGoToBuffer 5<cr>" },
+      { "<leader>6",  "<cmd>BufferLineGoToBuffer 6<cr>" },
+      { "<leader>7",  "<cmd>BufferLineGoToBuffer 7<cr>" },
+      { "<leader>8",  "<cmd>BufferLineGoToBuffer 8<cr>" },
+      { "<leader>9",  "<cmd>BufferLineGoToBuffer 9<cr>" },
+      { "<leader>-",  "<cmd>BufferLineGoToBuffer -1<cr>" },
     },
     opts = {
       options = {
