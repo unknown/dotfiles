df5590f63a3eed5fd98ba5d58b6f82c46865b8b5
David Mo <david@dmo.ooo>
2025-03-13 23:49:08 -0400

    more changes again

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,9 +4,20 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader><space>", "<cmd>Telescope find_files<cr>" },
-    { "<leader>ff",      "<cmd>Telescope find_files<cr>" },
     { "<leader>fg",      "<cmd>Telescope live_grep<cr>" },
     { "<leader>fb",      "<cmd>Telescope buffers<cr>" },
     { "<leader>fo",      "<cmd>Telescope oldfiles<cr>" },
   },
+  config = function()
+    require("telescope").setup({
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+        live_grep = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
+    })
+  end,
 }
