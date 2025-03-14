c08d16a0a52ad8bf118aa04d30ec4b0a256a3ee4
David Mo <david@dmo.ooo>
2025-03-14 13:19:20 -0400

    fix telescope grep ignoring hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,12 +9,20 @@ return {
     { "<leader>fo",      "<cmd>Telescope oldfiles<cr>" },
   },
   config = function()
-    require("telescope").setup({
+    local telescope = require("telescope")
+    local telescopeConfig = require("telescope.config")
+
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+    table.insert(vimgrep_arguments, "--hidden")
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
+
+    telescope.setup({
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+      },
       pickers = {
         find_files = {
-          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-        },
-        live_grep = {
           find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
         },
       },
